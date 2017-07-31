10717d9640d876edd1ce4b0e1ed3046ade0b0d8f
Silvanirzkya <151610173@smkassalaambandung.sch.id>
2017-07-31 16:35:50 +0700

    Crud Orangtua

--- routes/web.php
+++ routes/web.php
@@ -16,27 +16,28 @@ Route::get('/', function () {
 });
 
 
-Route::get('/orangtua', function () {
-    $or =App\orangtua::all();
-    foreach ($or as $data) {
-    	echo $data->nama_ibu." dengan ";
-    	echo $data->nama_ayah." <br>";
-    	foreach ($data->anak as $key => $value) {
-    		echo "<li>$value->nama</li>";
-    	}
+// Route::get('/orangtua', function () {
+//     $or =App\orangtua::all();
+//     foreach ($or as $data) {
+//     	echo $data->nama_ibu." dengan ";
+//     	echo $data->nama_ayah." <br>";
+//     	foreach ($data->anak as $key => $value) {
+//     		echo "<li>$value->nama</li>";
+//     	}
     
-    }
+//     }
 
-});
+// });
 
 
 Route::get('/percobaan','MyController@index');
-
 Route::get('/percobaan2','MyController@tampilan');
-
 Route::get('/percobaan3','MyController@tampilanmodel');
-
-
 Route::get('/admin', function () {
     return view('admin');
 });
+Auth::routes();
+Route::resource('/orangtua','OrangtuaController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
